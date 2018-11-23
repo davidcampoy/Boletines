e0ef7abc505fbb7acdd4b931210626817ea0c747
David <deividseventeen@gmail.com>
2018-11-23 11:17:01 +0100

    commit7

--- src/proyectos/Teclado.java
+++ src/proyectos/Teclado.java
@@ -8,7 +8,7 @@ public class Teclado {
 	
 	public static void main(String[] args) {
 		
-		short num = 7;
+		byte min = 10, max = 20;
 		
 		// Pruebas de las funciones (Retirar las barras de la prueba que se quiera comprobar).
 		
@@ -30,6 +30,10 @@ public class Teclado {
 		//System.out.println(readDouble());
 		
 		//System.out.println(compNum(num, Compare.Mayor));
+		
+		//System.out.println(rangNum(min, max, Range.MaxIncluded));
+		
+		System.out.println(choose("Elige una opción", "Opción 1", "Opción 2"));
 		
 	}
 	
@@ -582,13 +586,342 @@ public class Teclado {
 		
 	}
 	
-	public static byte rangNum(byte min, byte max) {
+	// Opciones para las funciones rangNum
+	public enum Range{
+		BothIncluded, BothExcluded, MinIncluded, MaxIncluded
+	}
+	
+	/*
+	 * 1.- Indicar un número mínimo y máximo.
+	 * 2.- Pedir al usuario un número comprendido entre esos números mínimo y máximo.
+	 * 	2.1.- Si el mínimo es mayor al máximo, se avisará al usuario mediante una excepción.
+	 * 	2.2.- Si el mínimo es menor o igual al máximo, se seguirá con la petición.
+	 * 3.- Devolver el valor.
+	 */
+	public static byte rangNum(byte min, byte max, Range option) {
 		
 		byte num = 0;
 		boolean end = true;
 		
-		System.out.println("Escribe un número comprendido entre " + min + "y " + max + ": ");
-		num = scan.nextByte();
+		if(min > max) { // Mostrar excepción.
+			throw new IllegalArgumentException("Error, el número mínimo no puede ser mayor que el máximo.");
+		}else {
+			do {
+				end = true;
+				try {
+					if(option == Range.BothIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos incluidos)" + ":");
+							num = scan.nextByte();
+						}while(num < min || num > max);
+					}else if(option == Range.BothExcluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos excluidos)" + ":");
+							num = scan.nextByte();
+						}while(num <= min || num >= max);
+					}else if(option == Range.MinIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (mínimo incluido)" + ":");
+							num = scan.nextByte();
+						}while(num < min || num >= max);
+					}else if(option == Range.MaxIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (máximo incluido)" + ":");
+							num = scan.nextByte();
+						}while(num <= min || num > max);
+					}
+				}catch(InputMismatchException e) {
+					end = false;
+				}finally {
+					scan.nextLine();
+				}
+			}while(!end);
+		}
+		return num;
+	}
+	
+	public static short rangNum(short min, short max, Range option) {
+		
+		short num = 0;
+		boolean end = true;
+		
+		if(min > max) { // Mostrar excepción.
+			throw new IllegalArgumentException("Error, el número mínimo no puede ser mayor que el máximo.");
+		}else {
+			do {
+				end = true;
+				try {
+					if(option == Range.BothIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos incluidos)" + ":");
+							num = scan.nextShort();
+						}while(num < min || num > max);
+					}else if(option == Range.BothExcluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos excluidos)" + ":");
+							num = scan.nextShort();
+						}while(num <= min || num >= max);
+					}else if(option == Range.MinIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (mínimo incluido)" + ":");
+							num = scan.nextShort();
+						}while(num < min || num >= max);
+					}else if(option == Range.MaxIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (máximo incluido)" + ":");
+							num = scan.nextShort();
+						}while(num <= min || num > max);
+					}
+				}catch(InputMismatchException e) {
+					end = false;
+				}finally {
+					scan.nextLine();
+				}
+			}while(!end);
+		}
+		return num;
+	}
+	
+	public static int rangNum(int min, int max, Range option) {
+		
+		int num = 0;
+		boolean end = true;
+		
+		if(min > max) { // Mostrar excepción.
+			throw new IllegalArgumentException("Error, el número mínimo no puede ser mayor que el máximo.");
+		}else {
+			do {
+				end = true;
+				try {
+					if(option == Range.BothIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos incluidos)" + ":");
+							num = scan.nextInt();
+						}while(num < min || num > max);
+					}else if(option == Range.BothExcluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos excluidos)" + ":");
+							num = scan.nextInt();
+						}while(num <= min || num >= max);
+					}else if(option == Range.MinIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (mínimo incluido)" + ":");
+							num = scan.nextInt();
+						}while(num < min || num >= max);
+					}else if(option == Range.MaxIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (máximo incluido)" + ":");
+							num = scan.nextInt();
+						}while(num <= min || num > max);
+					}
+				}catch(InputMismatchException e) {
+					end = false;
+				}finally {
+					scan.nextLine();
+				}
+			}while(!end);
+		}
+		return num;
+	}
+	
+	public static long rangNum(long min, long max, Range option) {
+		
+		long num = 0;
+		boolean end = true;
+		
+		if(min > max) { // Mostrar excepción.
+			throw new IllegalArgumentException("Error, el número mínimo no puede ser mayor que el máximo.");
+		}else {
+			do {
+				end = true;
+				try {
+					if(option == Range.BothIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos incluidos)" + ":");
+							num = scan.nextLong();
+						}while(num < min || num > max);
+					}else if(option == Range.BothExcluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos excluidos)" + ":");
+							num = scan.nextLong();
+						}while(num <= min || num >= max);
+					}else if(option == Range.MinIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (mínimo incluido)" + ":");
+							num = scan.nextLong();
+						}while(num < min || num >= max);
+					}else if(option == Range.MaxIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (máximo incluido)" + ":");
+							num = scan.nextLong();
+						}while(num <= min || num > max);
+					}
+				}catch(InputMismatchException e) {
+					end = false;
+				}finally {
+					scan.nextLine();
+				}
+			}while(!end);
+		}
+		return num;
+	}
+
+	public static float rangNum(float min, float max, Range option) {
+		
+		float num = 0;
+		boolean end = true;
+		
+		if(min > max) { // Mostrar excepción.
+			throw new IllegalArgumentException("Error, el número mínimo no puede ser mayor que el máximo.");
+		}else {
+			do {
+				end = true;
+				try {
+					if(option == Range.BothIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos incluidos)" + ":");
+							num = scan.nextFloat();
+						}while(num < min || num > max);
+					}else if(option == Range.BothExcluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos excluidos)" + ":");
+							num = scan.nextFloat();
+						}while(num <= min || num >= max);
+					}else if(option == Range.MinIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (mínimo incluido)" + ":");
+							num = scan.nextFloat();
+						}while(num < min || num >= max);
+					}else if(option == Range.MaxIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (máximo incluido)" + ":");
+							num = scan.nextFloat();
+						}while(num <= min || num > max);
+					}
+				}catch(InputMismatchException e) {
+					end = false;
+				}finally {
+					scan.nextLine();
+				}
+			}while(!end);
+		}
+		return num;
+	}
+
+	public static double rangNum(double min, double max, Range option) {
+		
+		double num = 0;
+		boolean end = true;
+		
+		if(min > max) { // Mostrar excepción.
+			throw new IllegalArgumentException("Error, el número mínimo no puede ser mayor que el máximo.");
+		}else {
+			do {
+				end = true;
+				try {
+					if(option == Range.BothIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos incluidos)" + ":");
+							num = scan.nextDouble();
+						}while(num < min || num > max);
+					}else if(option == Range.BothExcluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (ambos excluidos)" + ":");
+							num = scan.nextDouble();
+						}while(num <= min || num >= max);
+					}else if(option == Range.MinIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (mínimo incluido)" + ":");
+							num = scan.nextDouble();
+						}while(num < min || num >= max);
+					}else if(option == Range.MaxIncluded) {
+						do {
+							System.out.println("Introduce un número comprendido entre " + min + " y " + max + " (máximo incluido)" + ":");
+							num = scan.nextDouble();
+						}while(num <= min || num > max);
+					}
+				}catch(InputMismatchException e) {
+					end = false;
+				}finally {
+					scan.nextLine();
+				}
+			}while(!end);
+		}
+		return num;
+	}
+
+	/*
+	 * 1.- Mostrar tres mensajes al usuario.
+	 * 1.1.- EL primer mensaje será una pregunta o una petición.
+	 * 1.2.- Los demás mensajes serán las opciones uno y dos respectivamente.
+	 * 2.- El usuario deberá elegir la opción uno o dos indicando su número correspondiente.
+	 * 3.- Devolver true en caso de elegir la primera opción, y false en caso de la segunda.
+	 */
+	public static boolean choose(String msg1, String msg2, String msg3) {
+		
+		boolean option = false, end = true;
+		int numOption = 0;
+		
+		do {
+			end = true;
+			
+			try {
+				do {
+					System.out.println(msg1 + "\n\t1.-" + msg2 + "\n\t2.-" + msg3);
+					numOption = scan.nextInt();
+				}while(numOption < 1 || numOption > 2);
+				
+			}catch(InputMismatchException e) {
+				end = false;
+			}finally {
+				scan.nextLine();
+			}
+			
+		}while(!end);
+		
+		if(numOption == 1) {
+			option = true;
+			
+		}else if(numOption == 2) {
+			option = false;
+		}
+		
+		return option;
+		
+	}
+
+	/*
+	 * 1.- Mostrar un mensaje al usuario.
+	 * 2.- El usuario deberá indicar "si" o "no".
+	 * 3.- Devolver true en caso de indicar "si", y false en caso de indicar "no".
+	 */
+	public static boolean choose(String msg) {
+		
+		boolean option = false, end = true;
+		String answer;
+		
+		do {
+			
+			end = true;
+			
+			System.out.println(msg + "(s / n)");
+			answer = scan.nextLine();
+			
+			if(answer.equals("s")) {
+				option = true;
+			}else if(answer.equals("S")) {
+				option = true;
+			}else if(answer.equals("n")) {
+				option = false;
+			}else if(answer.equals("N")) {
+				option = false;
+			}else {
+				end = false;
+			}
+			
+		}while(!end);
+		
+		return option;
 		
 	}
 }
