9b0e046d2e3b50ebb2a63cac34a539e378d4738b
Unknown <deividseventeen@gmail.com>
2019-02-04 11:14:36 +0100

    commit24

---
+++ src/objetos/Persona.java
@@ -0,0 +1,24 @@
+package objetos;
+
+public class Persona {
+	
+	private String nombre;
+	private int edad;
+	private float altura;
+	private String ocupacion;
+	
+	String getNombre() {
+		return nombre;
+	}
+	
+	void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
